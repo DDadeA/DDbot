9340b9a82a0b090b75190fa81bbb5a56458fb26a
DDadeA <jyh2eh@gmail.com>
2024-01-08 23:12:42 +0900

    HOTFIX: Updater

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 # Auto update
 import os
 
-from .github import download_folder_from_github
+from bin.github import download_folder_from_github
 print(f"Current directory: {os.getcwd()}")
 download_folder_from_github("DDadeA", "DDbot", os.getcwd())
